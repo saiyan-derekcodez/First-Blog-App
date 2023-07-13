6898e2496c33a163fac06c62b3a85dedb280a9f8
saiyan-derekcodez <derekdavid866@gmail.com>
2023-07-13 11:07:50 +0100

    added postId to the schema and fixed date setting

--- models/comments.model.js
+++ models/comments.model.js
@@ -1,7 +1,28 @@
 import mongoose from "mongoose";
 
 const commentSchema = new mongoose.Schema({
-  author: {
+  fullname: {
     type: String,
+    required: [true, "Please input your fullname"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please input your name"],
+  },
+  comment: {
+    type: String,
+    required: [true, "please write a comment"],
+  },
+  postId: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
   },
 });
+
+const commentModel = mongoose.model("Comments", commentSchema);
+
+export default commentModel;
