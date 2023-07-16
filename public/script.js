8457bfc6d6a45ef0cfba1e9f8a78d2ec92fc96ad
saiyan-derekcodez <derekdavid866@gmail.com>
2023-07-16 09:42:03 +0100

    finished patch and delete method for the update and delete modal forms

--- public/script.js
+++ public/script.js
@@ -4,14 +4,12 @@ function getIdOfBlog(blogId) {
 
 async function DeleteBlog() {
   const blogId = document.getElementById("id").value;
-  console.log(blogId);
 
   const resp = await fetch(`${location.origin}/profile/${blogId}`, {
     method: "DELETE",
   });
 
   const data = await resp.json();
-  console.log(data);
 
   if (resp.status === 200) {
     location.reload();
@@ -21,34 +19,33 @@ async function DeleteBlog() {
 }
 
 async function getIdToUpdate(blogId) {
+  document.getElementById("id").value = blogId;
   // fetch the product information so I can show it in the form for update
   const resp = await fetch(`${location.origin}/profile/${blogId}`);
 
   const data = await resp.json();
-  console.log(data);
 
   if (resp.status !== 200) {
     alert(data.message);
-    return;
   }
 
   // IF IT GETS HERE THE INFORMATION WAS FOUND
-  // SO UPDATE THE PRODUCT UPDATE FIELDS
+  // SO UPDATE THE BLOG UPDATE FIELDS
   document.querySelector("input[name='updatedTitle']").value = data.blog.title;
   document.querySelector("input[name='updatedImage']").value = data.blog.image;
-  document.querySelector("input[name='updatedCategory']").value =
-    data.blog.category;
-  document.querySelector("input[name='updatedContent']").value =
+  document.querySelector("input[name='updatedImage']").value = data.blog.image;
+  document.querySelector("input[name='blogId']").value = blogId;
+  document.querySelector("textarea[name='updatedContent']").value =
     data.blog.content;
 }
 
 async function updateProduct() {
   // RETRIEVE THE UPDATED INFO
   const update = {
-    name: document.querySelector("input[name='updatedName']").value,
-    img: document.querySelector("input[name='updatedImg']").value,
-    price: document.querySelector("input[name='updatedPrice']").value,
-    quantity: document.querySelector("input[name='updatedQuantity']").value,
+    name: document.querySelector("input[name='updatedTitle']']").value,
+    img: document.querySelector("input[name='updatedImage']").value,
+    price: document.querySelector("input[name='updatedCategory']").value,
+    quantity: document.querySelector("textarea[name='updatedContent']").value,
   };
 
   const blogId = document.querySelector("input[name='blogId']").value;
